b31ad3be9a2a646b532a5fe7eaf585e2127c939d
Takemonist <75937621+Takemonist@users.noreply.github.com>
2023-01-13 16:16:39 +0900

    add

--- main.cpp
+++ main.cpp
@@ -14,11 +14,6 @@ int main()
 {
     // Initialise the digital pin LED1 as an output
     DigitalOut led(LED1);
-    #if __cplusplus == 201103L
-        static_assert(false, "C++14 under");
-    #elif __cplusplus > 201103L
-        static_assert(false, "C++17 more");
-    #endif
     while (true) {
         if(int i = 0;i < 4){
         led = !led;
