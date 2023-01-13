b8f1ac2ac5a40ed8a8d6300c872e006db85d412b
Takemonist <75937621+Takemonist@users.noreply.github.com>
2023-01-13 14:53:41 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,9 @@
 
 #include "mbed.h"
 #include "platform/mbed_thread.h"
-#include <concepts>
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE_MS                                                    500
-
-template<typename T>
-void f(T x){}
 
 int main()
 {
@@ -19,8 +15,9 @@ int main()
     DigitalOut led(LED1);
 
     while (true) {
-        f(1);
+        if(int i = 0;i < 4){
         led = !led;
+        }
         thread_sleep_for(BLINKING_RATE_MS);
     }
 }
